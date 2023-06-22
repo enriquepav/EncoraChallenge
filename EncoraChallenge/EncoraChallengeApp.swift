de13263a8585c199e924f7a06f81923f44964118
Enrique Alata <e.alatavences@gmail.com>
2023-06-22 12:09:19 -0500

    Initial Commit

---
+++ EncoraChallenge/EncoraChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncoraChallengeApp.swift
+//  EncoraChallenge
+//
+//  Created by MAC1DIGITAL20 on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EncoraChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
